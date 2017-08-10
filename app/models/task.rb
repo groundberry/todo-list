8efe38e4cda12d50561b2b2c8bc06e3608f5a226
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-08-10 11:21:09 -0700

    Add validations for task presence and length

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :task, presence: true,
+                    length: { minimum: 3 }
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid task with task name length greater than or equal to 3 characters" do
+    task = Task.new(task: 'Run')
+    assert task.valid?
+  end
+
+  test "invalid task without task name" do
+    task = Task.new()
+    refute task.valid?, 'task is not valid without a task name'
+    assert_not_nil task.errors[:task], 'no validation error for task name present'
+  end
+
+  test "invalid task with task name length less than 3 characters" do
+    task = Task.new(task: 'Fo')
+    refute task.valid?, 'task is not valid with length less than 3 characters'
+    assert_not_nil task.errors[:task], 'no validation error for task name length'
+  end
 end
